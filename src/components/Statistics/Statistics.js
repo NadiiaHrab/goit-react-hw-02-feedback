4009b5056b34d4f501b4cc5a2c09aa5d1894f031
DemouserNG <108471967+NadiiaHrab@users.noreply.github.com>
2023-02-02 18:55:00 +0200

    fixd

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -4,15 +4,13 @@ import css from "./Statistics.module.css";
 function Statistics({ good, neutral, bad, total, positivePercentage }) {
     return (
         <>
-    
     <div className = {css.statisticsWrap}>
     <p className={css.text}>Good: {good}</p>
     <p className={css.text}>Neutral: {neutral}</p>
     <p className={css.text}>Bad: {bad}</p>
     <p className={css.text}>Total: {total}</p>
     <p className={css.text}>Positive Feedback: {positivePercentage}%</p>
-    </div> 
-        
+    </div>       
  </> 
     )
 }
